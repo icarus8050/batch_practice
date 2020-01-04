c9898b25b730f33030c0fabddfe057dec306ad0d
icarus8050 <icarus8050@naver.com>
2020-01-04 19:15:39 +0900

    Test

---
+++ src/test/java/com/icarus/batch/community/batch/InactiveUserJobTest.java
@@ -0,0 +1,36 @@
+package com.icarus.batch.community.batch;
+
+import com.icarus.batch.domain.enums.UserStatus;
+import com.icarus.batch.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class InactiveUserJobTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void 휴먼_회원_전환_테스트() throws Exception {
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+        assertEquals(0, userRepository.findByUpdatedDateBeforeAndStatusEquals(
+                LocalDateTime.now().minusYears(1), UserStatus.ACTIVE).size());
+    }
+}
