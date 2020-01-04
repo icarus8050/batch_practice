ce061c23dda8e415ec3419220c9cc48ddba18a26
icarus8050 <icarus8050@naver.com>
2020-01-04 17:18:08 +0900

    패키지 변경

---
+++ src/main/java/com/icarus/batch/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.icarus.batch.repository;
+
+import com.icarus.batch.domain.User;
+import com.icarus.batch.domain.enums.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByUpdatedDateBeforeAndStatusEquals(LocalDateTime localDateTime,
+                                                      UserStatus status);
+}
